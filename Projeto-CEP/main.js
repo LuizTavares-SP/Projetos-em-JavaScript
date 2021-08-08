52303d21d3cc3dcb0230d72ab3db6856da1d7414
Luiz Tavares <81531056+LuizTavares-SP@users.noreply.github.com>
2021-08-08 10:47:55 -0300

    Exercício Projeto-CEP

---
+++ Projeto-CEP/main.js
@@ -0,0 +1,45 @@
+//'use strict';
+/*
+const limparFormulario = (endereco) => {
+    document.getElementById('endereco').value = "";
+    document.getElementById('bairro').value = "";
+    document.getElementById('cidade').value = "";
+    document.getElementById('estado').value = "";
+}
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+const pesquisarCep = async() => {
+    limparFormulario();
+    const cep = document.getElementById('cep').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+
+    if (cepValido(cep)){
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if (endereco.hasOwnProperty('erro') ){
+            document.getElementById('endereco').value = "CEP NÃO ENCONTRADO!!";
+        }else{
+            preencherFormulario(endereco);
+        }
+    }else{
+        document.getElementById('endereco').value = "CEP INCORRETO!!"
+    }
+}
+*/
+
+const pesquisarCep = () => {
+    var cep = document.querySelector('cep').value;
+    console.log(cep);
+}
+
+document.getElementById('cep').addEventListener("focus",pesquisarCep);
